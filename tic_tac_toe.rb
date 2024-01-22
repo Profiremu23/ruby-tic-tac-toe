0ea0f0c68c70d138d70e85c95c9e3f9c488525aa
Profiremu23 <profirdani23@gmail.com>
2024-01-22 20:04:10 +0100

    renamed the tic tac toe file

---
+++ tic_tac_toe.rb
@@ -0,0 +1,11 @@
+# Creating a 3x3 game board
+class Board
+  def board_assembler
+    p Array.new(3, ' ') { Array.new(3, ' ') }
+  end
+end
+
+board = Board.new.board_assembler
+p board[0][1] = 'X'
+p board[1][0] = 'O'
+p board
