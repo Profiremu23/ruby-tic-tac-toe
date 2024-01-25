54511830919bb25b85c84f4dcb6bb265a42e2bdb
Profiremu23 <profirdani23@gmail.com>
2024-01-25 20:05:16 +0100

    Redesigned the game board structure

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,45 @@
-# Creating a 3x3 game board
-class Board
-  def board_assembler
-    p Array.new(3, ' ') { Array.new(3, ' ') }
+# frozen_string_literal: true
+
+# Defining a cell on the game board
+class Cell
+  attr_accessor :symbol
+
+  def initialize
+    @symbol = nil
+  end
+
+  def free?
+    symbol.nil?
+  end
+
+  def to_s
+    symbol || ' '
   end
 end
 
-board = Board.new.board_assembler
-p board[0][1] = 'X'
-p board[1][0] = 'O'
-p board
+# Creating a 3x3 game board
+class GameBoard
+  attr_reader :board_state
+
+  def initialize
+    @board_state = Array.new(3) { Array.new(3) { Cell.new } }
+  end
+
+  def cell_free?(row, column)
+    board_state[row][column].free?
+  end
+
+  def update_cell(row, column, symbol)
+    board_state[row][column].symbol = symbol
+  end
+
+  def print_board
+    print "\n| #{board_state[0][0]} | #{board_state[0][1]} | #{board_state[0][2]} |\n| #{board_state[1][0]} | #{board_state[1][1]} | #{board_state[1][2]} |\n| #{board_state[2][0]} | #{board_state[2][1]} | #{board_state[2][2]} |\n"
+  end
+end
+
+table = GameBoard.new
+table.cell_free?(0, 1)
+table.update_cell(0, 1, 'X')
+table.cell_free?(0, 1)
+table.print_board
