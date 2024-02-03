b23daf0b8769acbc1576b690c8d11cfc713ddfde
Profiremu23 <profirdani23@gmail.com>
2024-02-03 19:53:01 +0100

    Split the winning_condition into horizontal, vertical and diagonal conditions, also added functional turn variable

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -19,10 +19,13 @@ end
 
 # Creating a 3x3 game board
 class GameBoard
+  attr_accessor :turn
   attr_reader :board_state
 
   def initialize
     @board_state = Array.new(3) { Array.new(3) { Cell.new } }
+    @turn = 0
+    @character = 'X'
   end
 
   def cell_free?(row, column)
@@ -34,48 +37,68 @@ class GameBoard
   end
 
   def print_board
-    print "| #{board_state[0][0]} | #{board_state[0][1]} | #{board_state[0][2]} |
+    print "\n| #{board_state[0][0]} | #{board_state[0][1]} | #{board_state[0][2]} |
 | #{board_state[1][0]} | #{board_state[1][1]} | #{board_state[1][2]} |
 | #{board_state[2][0]} | #{board_state[2][1]} | #{board_state[2][2]} |\n\n"
   end
-end
 
-# Defining the winning conditions
-class WinningConditions < GameBoard
-  def winning_condition
-    # Horizontal winning patterns
-    if (board_state[0][0] = 'X' && board_state[0][1] = 'X' && board_state[0][2] = 'X') || (board_state[0][0] = 'O' && board_state[0][1] = 'O' && board_state[0][2] = 'O')
+  # Horizontal winning patterns
+  def horizontal_winning_condition
+    if (board_state[0][0] == 'X' && board_state[0][1] == 'X' && board_state[0][2] == 'X') || (board_state[0][0] == 'O' && board_state[0][1] == 'O' && board_state[0][2] == 'O')
       puts 'Congratulations! You won!'
-    elsif (board_state[1][0] = 'X' && board_state[1][1] = 'X' && board_state[1][2] = 'X') || (board_state[1][0] = 'O' && board_state[1][1] = 'O' && board_state[1][2] = 'O')
+    elsif (board_state[1][0] == 'X' && board_state[1][1] == 'X' && board_state[1][2] == 'X') || (board_state[1][0] == 'O' && board_state[1][1] == 'O' && board_state[1][2] == 'O')
       puts 'Congratulations! You won!'
-    elsif (board_state[2][0] = 'X' && board_state[2][1] = 'X' && board_state[2][2] = 'X') || (board_state[2][0] = 'O' && board_state[2][1] = 'O' && board_state[2][2] = 'O')
-      puts 'Congratulations! You won!'
-    # Vertical winning patterns
-    elsif (board_state[0][0] = 'X' && board_state[1][0] = 'X' && board_state[2][0] = 'X') || (board_state[0][0] = 'O' && board_state[1][0] = 'O' && board_state[2][0] = 'O')
-      puts 'Congratulations! You won!'
-    elsif (board_state[0][1] = 'X' && board_state[1][1] = 'X' && board_state[2][1] = 'X') || (board_state[0][1] = 'O' && board_state[1][1] = 'O' && board_state[2][1] = 'O')
-      puts 'Congratulations! You won!'
-    elsif (board_state[0][2] = 'X' && board_state[1][2] = 'X' && board_state[2][2] = 'X') || (board_state[0][2] = 'O' && board_state[1][2] = 'O' && board_state[2][2] = 'O')
-      puts 'Congratulations! You won!'
-      # Diagonal winning patterns
-    elsif (board_state[0][0] = 'X' && board_state[1][1] = 'X' && board_state[2][2] = 'X') || (board_state[0][0] = 'O' && board_state[1][1] = 'O' && board_state[2][2] = 'O')
-      puts 'Congratulations! You won!'
-    elsif (board_state[2][0] = 'X' && board_state[1][1] = 'X' && board_state[0][2] = 'X') || (board_state[2][0] = 'O' && board_state[1][1] = 'O' && board_state[0][2] = 'O')
+    elsif (board_state[2][0] == 'X' && board_state[2][1] == 'X' && board_state[2][2] == 'X') || (board_state[2][0] == 'O' && board_state[2][1] == 'O' && board_state[2][2] == 'O')
       puts 'Congratulations! You won!'
     else
+      puts self.vertical_winning_condition
+    end
+  end
+
+  # Vertical winning patterns
+  def vertical_winning_condition
+    if (board_state[0][0] == 'X' && board_state[1][0] == 'X' && board_state[2][0] == 'X') || (board_state[0][0] == 'O' && board_state[1][0] == 'O' && board_state[2][0] == 'O')
+      puts 'Congratulations! You won!'
+    elsif (board_state[0][1] == 'X' && board_state[1][1] == 'X' && board_state[2][1] == 'X') || (board_state[0][1] == 'O' && board_state[1][1] == 'O' && board_state[2][1] == 'O')
+      puts 'Congratulations! You won!'
+    elsif (board_state[0][2] == 'X' && board_state[1][2] == 'X' && board_state[2][2] == 'X') || (board_state[0][2] == 'O' && board_state[1][2] == 'O' && board_state[2][2] == 'O')
+      puts 'Congratulations! You won!'
+    else
+      puts self.diagonal_winning_condition
+    end
+  end
+
+  # Diagonal winning patterns + drawing and game continuation logic
+  def diagonal_winning_condition
+    if (board_state[0][0] == 'X' && board_state[1][1] == 'X' && board_state[2][2] == 'X') || (board_state[0][0] == 'O' && board_state[1][1] == 'O' && board_state[2][2] == 'O')
+      puts 'Congratulations! You won!'
+    elsif (board_state[2][0] == 'X' && board_state[1][1] == 'X' && board_state[0][2] == 'X') || (board_state[2][0] == 'O' && board_state[1][1] == 'O' && board_state[0][2] == 'O')
+      puts 'Congratulations! You won!'
+    elsif turn > 9
       puts "It's a draw!"
+    else
+      puts 'Game continues!'
+      @turn = turn + 1
     end
   end
 end
 
 # Defining the game conditions
-class Game < WinningConditions
+class Game < GameBoard
+  def current_player
+    if turn.odd?
+      character = 'X'
+    else
+      character = 'O'
+    end
+  end
 end
 
 table = Game.new
-table.update_cell(2, 0, 'O')
+table.update_cell(2, 0, 'X')
 table.print_board
-table.update_cell(1, 1, 'O')
-table.update_cell(0, 2, 'O')
+table.update_cell(1, 1, 'X')
+table.update_cell(0, 2, 'X')
 table.print_board
-table.winning_condition
+table.horizontal_winning_condition
+table.horizontal_winning_condition
