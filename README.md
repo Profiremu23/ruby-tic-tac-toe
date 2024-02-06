39128c5193c64dfc58752aaabae8c61f8cf636f7
Profiremu23 <profirdani23@gmail.com>
2024-02-06 16:43:53 +0100

    Final touches

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ruby-tic-tac-toe
-My fifth Ruby projectmade for the Odin Project (TOP) and my first OOP software I ever coded
+This is my fifth Ruby project made for [the Odin Project (TOP)](https://theodinproject.com) and it is my first OOP software I have ever coded. It is a faithful replica of Tic-Tac-Toe, a classical game that many of us have played it even once in our lives. By starting the game file of tic_tac_toe.rb, you can play a game of Tic-Tac-Toe with one of your friends (or with yourself if you don't have any friends). It is a complete recreation with all of the necessary game mechanics to enjoy it at it's full potential.
+## How do you run it?
+To run this game just like many other Ruby software, use Git or your local Linux/MacOS terminal, navigate to ../ruby-tic-tac-toe, then enter ruby tic_tac_toe.rb as mentioned above.
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -24,8 +24,11 @@ class GameBoard
 
   def initialize
     @board_state = Array.new(3) { Array.new(3) { Cell.new } }
-    @turn = 1
+    @row = 0
+    @column = 0
     @character = 'X'
+    @player = 'Player One'
+    @turn = 1
   end
 
   def cell_free?(row, column)
@@ -37,85 +40,141 @@ class GameBoard
       board_state[row][column].symbol = symbol
     else
       puts "The entered cell for row #{row} and column #{column} is already taken!"
-      @turn = turn - 1
+      @turn -= 1
     end
   end
 
   def print_board
     print "\n| #{board_state[0][0]} | #{board_state[0][1]} | #{board_state[0][2]} |
 | #{board_state[1][0]} | #{board_state[1][1]} | #{board_state[1][2]} |
-| #{board_state[2][0]} | #{board_state[2][1]} | #{board_state[2][2]} |\n\n"
+| #{board_state[2][0]} | #{board_state[2][1]} | #{board_state[2][2]} |\n"
   end
 
-  # Horizontal winning patterns
-  def horizontal_winning_condition
-    if (board_state[0][0].symbol == 'X' && board_state[0][1].symbol == 'X' && board_state[0][2].symbol == 'X') || (board_state[0][0].symbol == 'O' && board_state[0][1].symbol == 'O' && board_state[0][2].symbol == 'O')
-      puts 'Congratulations! You won!'
-    elsif (board_state[1][0].symbol == 'X' && board_state[1][1].symbol == 'X' && board_state[1][2].symbol == 'X') || (board_state[1][0].symbol == 'O' && board_state[1][1].symbol == 'O' && board_state[1][2].symbol == 'O')
-      puts 'Congratulations! You won!'
-    elsif (board_state[2][0].symbol == 'X' && board_state[2][1].symbol == 'X' && board_state[2][2].symbol == 'X') || (board_state[2][0].symbol == 'O' && board_state[2][1].symbol == 'O' && board_state[2][2].symbol == 'O')
-      puts 'Congratulations! You won!'
+  # Winning message
+  def winning_message
+    puts "Congratulations! #{player} won!"
+  end
+
+  # Horizontal winning patterns (primary is marked with X, secondary is with O)
+  def primary_horizontal_winning_condition
+    if board_state[0][0].symbol == 'X' && board_state[0][1].symbol == 'X' && board_state[0][2].symbol == 'X'
+      winning_message
+    elsif board_state[1][0].symbol == 'X' && board_state[1][1].symbol == 'X' && board_state[1][2].symbol == 'X'
+      winning_message
+    elsif board_state[2][0].symbol == 'X' && board_state[2][1].symbol == 'X' && board_state[2][2].symbol == 'X'
+      winning_message
     else
-      puts self.vertical_winning_condition
+      secondary_horizontal_winning_condition
+    end
+  end
+
+  def secondary_horizontal_winning_condition
+    if board_state[0][0].symbol == 'O' && board_state[0][1].symbol == 'O' && board_state[0][2].symbol == 'O'
+      winning_message
+    elsif board_state[1][0].symbol == 'O' && board_state[1][1].symbol == 'O' && board_state[1][2].symbol == 'O'
+      winning_message
+    elsif board_state[2][0].symbol == 'O' && board_state[2][1].symbol == 'O' && board_state[2][2].symbol == 'O'
+      winning_message
+    else
+      primary_vertical_winning_condition
     end
   end
 
   # Vertical winning patterns
-  def vertical_winning_condition
-    if (board_state[0][0].symbol == 'X' && board_state[1][0].symbol == 'X' && board_state[2][0].symbol == 'X') || (board_state[0][0].symbol == 'O' && board_state[1][0].symbol == 'O' && board_state[2][0].symbol == 'O')
-      puts 'Congratulations! You won!'
-    elsif (board_state[0][1].symbol == 'X' && board_state[1][1].symbol == 'X' && board_state[2][1].symbol == 'X') || (board_state[0][1].symbol == 'O' && board_state[1][1].symbol == 'O' && board_state[2][1].symbol == 'O')
-      puts 'Congratulations! You won!'
-    elsif (board_state[0][2].symbol == 'X' && board_state[1][2].symbol == 'X' && board_state[2][2].symbol == 'X') || (board_state[0][2].symbol == 'O' && board_state[1][2].symbol == 'O' && board_state[2][2].symbol == 'O')
-      puts 'Congratulations! You won!'
+  def primary_vertical_winning_condition
+    if board_state[0][0].symbol == 'X' && board_state[1][0].symbol == 'X' && board_state[2][0].symbol == 'X'
+      winning_message
+    elsif board_state[0][1].symbol == 'X' && board_state[1][1].symbol == 'X' && board_state[2][1].symbol == 'X'
+      winning_message
+    elsif board_state[0][2].symbol == 'X' && board_state[1][2].symbol == 'X' && board_state[2][2].symbol == 'X' 
+      winning_message
     else
-      puts self.diagonal_winning_condition
+      secondary_vertical_winning_condition
+    end
+  end
+
+  def secondary_vertical_winning_condition
+    if board_state[0][0].symbol == 'O' && board_state[1][0].symbol == 'O' && board_state[2][0].symbol == 'O'
+      winning_message
+    elsif board_state[0][1].symbol == 'O' && board_state[1][1].symbol == 'O' && board_state[2][1].symbol == 'O'
+      winning_message
+    elsif board_state[0][2].symbol == 'O' && board_state[1][2].symbol == 'O' && board_state[2][2].symbol == 'O'
+      winning_message
+    else
+      primary_diagonal_winning_condition
     end
   end
 
   # Diagonal winning patterns + drawing and game continuation logic
-  def diagonal_winning_condition
-    if (board_state[0][0].symbol == 'X' && board_state[1][1].symbol == 'X' && board_state[2][2].symbol == 'X') || (board_state[0][0].symbol == 'O' && board_state[1][1].symbol == 'O' && board_state[2][2].symbol == 'O')
-      puts 'Congratulations! You won!'
-    elsif (board_state[2][0].symbol == 'X' && board_state[1][1].symbol == 'X' && board_state[0][2].symbol == 'X') || (board_state[2][0].symbol == 'O' && board_state[1][1].symbol == 'O' && board_state[0][2].symbol == 'O')
-      puts 'Congratulations! You won!'
-    elsif turn > 9
+  def primary_diagonal_winning_condition
+    if board_state[0][0].symbol == 'X' && board_state[1][1].symbol == 'X' && board_state[2][2].symbol == 'X'
+      winning_message
+    elsif board_state[2][0].symbol == 'X' && board_state[1][1].symbol == 'X' && board_state[0][2].symbol == 'X'
+      winning_message
+    else
+      secondary_diagonal_winning_condition
+    end
+  end
+
+  def secondary_diagonal_winning_condition
+    if board_state[0][0].symbol == 'O' && board_state[1][1].symbol == 'O' && board_state[2][2].symbol == 'O'
+      puts "Congratulations! #{player} won!"
+    elsif board_state[2][0].symbol == 'O' && board_state[1][1].symbol == 'O' && board_state[0][2].symbol == 'O'
+      puts "Congratulations! #{player} won!"
+    else
+      winless_conditions
+    end
+  end
+
+  # Game continuation and drawing logic
+  def winless_conditions
+    if turn > 9
       puts "It's a draw!"
     else
-      @turn = turn + 1
-      self.game_function
+      @turn += 1
+      game_function
     end
   end
 end
 
 # Defining the game conditions
 class Game < GameBoard
-  def game_function
-    puts "Turn #{turn}"
-
+  def player
     if turn.odd?
-      character = 'X'
+      @character = 'X'
+      @player = 'Player One'
     else
-      character = 'O'
+      @character = 'O'
+      @player = 'Player Two'
     end
+  end
 
+  def row
     puts 'Please enter a number for your chosen row:'
-    row = gets.chomp.to_i
-    until row.negative? == false && row < 3
+    @row = gets.chomp.to_i
+    until @row.negative? == false && @row < 3
       puts "The given value for the board's row is out of range!"
-      row = gets.chomp.to_i
+      @row = gets.chomp.to_i
     end
+  end
 
+  def column
     puts 'Please enter a number for your chosen column:'
-    column = gets.chomp.to_i
-    until column.negative? == false && column < 3
+    @column = gets.chomp.to_i
+    until @column.negative? == false && @column < 3
       puts "The given value for the board's column is out of range!"
-      column = gets.chomp.to_i
+      @column = gets.chomp.to_i
     end
+  end
 
-    self.update_cell(row, column, character)
-    puts self.print_board
-    self.horizontal_winning_condition
+  def game_function
+    player
+    puts "Turn #{@turn} for #{@player} (#{@character})"
+    row
+    column
+    update_cell(@row, @column, @character)
+    puts print_board
+    primary_horizontal_winning_condition
   end
 end
 
